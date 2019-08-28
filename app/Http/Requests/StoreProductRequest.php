25266ada5e7e029472ac4357700ea48555a4fbc4
Dormynion <dlorunfemi@gmail.com>
2019-08-28 08:49:45 +0100

    up

--- app/Http/Requests/StoreProductRequest.php
+++ app/Http/Requests/StoreProductRequest.php
@@ -1,54 +1,54 @@
 <?php
 
-namespace App\Http\Requests;
+// namespace App\Http\Requests;
 
-use App\Property;
-use Illuminate\Foundation\Http\FormRequest;
+// use App\Property;
+// use Illuminate\Foundation\Http\FormRequest;
 
-class StorePropertyRequest extends FormRequest
-{
-    public function authorize()
-    {
-        return \Gate::allows('property_create');
-    }
+// class StorePropertyRequest extends FormRequest
+// {
+//     public function authorize()
+//     {
+//         return \Gate::allows('property_create');
+//     }
 
-    public function rules()
-    {
-        return [
-            'landlord_id'   => [
-                'required',
-                'integer',
-            ],
-            'categories.*'  => [
-                'integer',
-            ],
-            'categories'    => [
-                'required',
-                'array',
-            ],
-            'is_new'        => [
-                'required',
-            ],
-            'name'          => [
-                'required',
-            ],
-            'state'         => [
-                'required',
-            ],
-            'is_bank'       => [
-                'required',
-            ],
-            'is_dormant'    => [
-                'required',
-            ],
-            'officer_id'    => [
-                'required',
-                'integer',
-            ],
-            'created_by_id' => [
-                'required',
-                'integer',
-            ],
-        ];
-    }
-}
+//     public function rules()
+//     {
+//         return [
+//             'landlord_id'   => [
+//                 'required',
+//                 'integer',
+//             ],
+//             'categories.*'  => [
+//                 'integer',
+//             ],
+//             'categories'    => [
+//                 'required',
+//                 'array',
+//             ],
+//             'is_new'        => [
+//                 'required',
+//             ],
+//             'name'          => [
+//                 'required',
+//             ],
+//             'state'         => [
+//                 'required',
+//             ],
+//             'is_bank'       => [
+//                 'required',
+//             ],
+//             'is_dormant'    => [
+//                 'required',
+//             ],
+//             'officer_id'    => [
+//                 'required',
+//                 'integer',
+//             ],
+//             'created_by_id' => [
+//                 'required',
+//                 'integer',
+//             ],
+//         ];
+//     }
+// }
--- app/Http/Requests/UpdateProductRequest.php
+++ app/Http/Requests/UpdateProductRequest.php
@@ -1,54 +1,54 @@
 <?php
 
-namespace App\Http\Requests;
+// namespace App\Http\Requests;
 
-use App\Property;
-use Illuminate\Foundation\Http\FormRequest;
+// use App\Property;
+// use Illuminate\Foundation\Http\FormRequest;
 
-class UpdatePropertyRequest extends FormRequest
-{
-    public function authorize()
-    {
-        return \Gate::allows('property_edit');
-    }
+// class UpdatePropertyRequest extends FormRequest
+// {
+//     public function authorize()
+//     {
+//         return \Gate::allows('property_edit');
+//     }
 
-    public function rules()
-    {
-        return [
-            'landlord_id'  => [
-                'required',
-                'integer',
-            ],
-            'categories.*' => [
-                'integer',
-            ],
-            'categories'   => [
-                'required',
-                'array',
-            ],
-            'is_new'       => [
-                'required',
-            ],
-            'name'         => [
-                'required',
-            ],
-            'state'        => [
-                'required',
-            ],
-            'is_bank'      => [
-                'required',
-            ],
-            'is_dormant'   => [
-                'required',
-            ],
-            'dormant_date' => [
-                'date_format:' . config('panel.date_format') . ' ' . config('panel.time_format'),
-                'nullable',
-            ],
-            'officer_id'   => [
-                'required',
-                'integer',
-            ],
-        ];
-    }
-}
+//     public function rules()
+//     {
+//         return [
+//             'landlord_id'  => [
+//                 'required',
+//                 'integer',
+//             ],
+//             'categories.*' => [
+//                 'integer',
+//             ],
+//             'categories'   => [
+//                 'required',
+//                 'array',
+//             ],
+//             'is_new'       => [
+//                 'required',
+//             ],
+//             'name'         => [
+//                 'required',
+//             ],
+//             'state'        => [
+//                 'required',
+//             ],
+//             'is_bank'      => [
+//                 'required',
+//             ],
+//             'is_dormant'   => [
+//                 'required',
+//             ],
+//             'dormant_date' => [
+//                 'date_format:' . config('panel.date_format') . ' ' . config('panel.time_format'),
+//                 'nullable',
+//             ],
+//             'officer_id'   => [
+//                 'required',
+//                 'integer',
+//             ],
+//         ];
+//     }
+// }
