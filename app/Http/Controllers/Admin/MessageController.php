a30fbbdcf132f8b555045346c43435b3bbabe8e4
Dormynion <dlorunfemi@gmail.com>
2019-08-30 09:47:21 +0100

    s

--- app/Http/Controllers/Admin/MessageController.php
+++ app/Http/Controllers/Admin/MessageController.php
@@ -79,15 +79,15 @@ class MessageController extends Controller
         return response()->json($message);
     }
 
-	// public function fetchMessages()
-    // {
-    //     abort_unless(\Gate::allows('message_access'), 403);
+	public function fetchMessages()
+    {
+        abort_unless(\Gate::allows('message_access'), 403);
 
-    //     $message = Message::with('user')->get();
-    //     // dd($message);
+        $message = Message::with('user')->get();
+        // dd($message);
 
-	//   	return view('admin.messages.index', compact('message'));
-    // }
+	  	return view('admin.messages.index', compact('message'));
+    }
 
     // public function privateMessages(User $user)
     // {
@@ -101,28 +101,28 @@ class MessageController extends Controller
     //     return $privateCommunication;
     // }
 
-    // public function sendMessage(Request $request)
-    // {
+    public function sendMessage(Request $request)
+    {
 
 
-    //     if(request()->has('file')){
-    //         $filename = request('file')->store('chat');
-    //         $message=Message::create([
-    //             'user_id' => request()->user()->id,
-    //             'image' => $filename,
-    //             'receiver_id' => request('receiver_id')
-    //         ]);
-    //     }else{
-    //         $message = auth()->user()->messages()->create(['message' => $request->message]);
+        if(request()->has('file')){
+            $filename = request('file')->store('chat');
+            $message=Message::create([
+                'user_id' => request()->user()->id,
+                'image' => $filename,
+                'receiver_id' => request('receiver_id')
+            ]);
+        }else{
+            $message = auth()->user()->messages()->create(['message' => $request->message]);
 
-    //     }
+        }
 
 
-    //     broadcast(new MessageSent(auth()->user(),$message->load('user')))->toOthers();
+        broadcast(new MessageSent(auth()->user(),$message->load('user')))->toOthers();
 
-    //     return response(['status'=>'Message sent successfully','message'=>$message]);
+        return response(['status'=>'Message sent successfully','message'=>$message]);
 
-    // }
+    }
 
     // public function sendPrivateMessage(Request $request,User $user)
     // {
